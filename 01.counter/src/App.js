c4a51a82b60b28ff779ff0603f8248ea5df0d0fe
Mariana Torres <martorres612@gmail.com>
2023-08-22 19:44:48 -0600

    Traer a nuestro componente counter

--- 01.counter/src/App.js
+++ 01.counter/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Counter from './Counter';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <Counter initCount ={0} />
        
       </header>
     </div>
